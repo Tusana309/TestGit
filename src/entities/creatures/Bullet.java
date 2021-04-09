bbcd376f0f4f956af830e34114572f575f00a28f
@nhTuan <tuan30vh@gmail.com>
2021-04-09 23:28:07 +0700

    update clean up bullet & sword -> weapon

--- src/entities/creatures/Bullet.java
+++ src/entities/creatures/Bullet.java
@@ -1,122 +1,36 @@
 package entities.creatures;
 
 import entities.Entity;
+import entities.creatures.weapon.Weapon;
 import game.Handler;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.media.MediaPlayer;
 import settings.Settings;
 import sounds.Sound;
 
-public class Bullet extends Creature{
+public class Bullet extends Weapon {
 
-    private boolean isFired = false;
-    private int xLong = 0, yLong = 0;
-
-    public Bullet(Handler handler, Image image, double x, double y, int damage, int direction) {
-        super(handler, image, x, y, 20, 21, damage);
-        this.direction = direction;
-
-        setSpeed(30);
-
-        bounds.setX(4);
-        bounds.setY(4);
-        bounds.setWidth(20);
-        bounds.setHeight(20);
-
-    }
-
-    @Override
-    public void move() {
-        moveX();
-        moveY();
-    }
-
-    public void moveX(){
-        if(xMove > 0){ //Moving right
-            int tx = (int) (x + bounds.getX() + bounds.getWidth()) / Settings.TILE_WIDTH;
-
-            if(!collisionWithTile(tx, (int) (y + bounds.getY()) / Settings.TILE_HEIGHT) &&
-                    !collisionWithTile(tx, (int) (y + bounds.getY() + bounds.getHeight()) / Settings.TILE_HEIGHT)){
-                x += xMove/2;
-                xLong += xMove/2;
-            } else {
-                die();
-            }
-        } else if (xMove < 0){ //Moving left
-            int tx = (int) (x + bounds.getX()) / Settings.TILE_WIDTH;
-
-            if (!collisionWithTile(tx, (int) (y + bounds.getY()) / Settings.TILE_HEIGHT) &&
-                    !collisionWithTile(tx, (int) (y + bounds.getY() + bounds.getHeight()) / Settings.TILE_HEIGHT)) {
-                x += xMove/2;
-                xLong += xMove/2;
-            } else {
-                die();
-            }
-        }
-    }
-
-    public void moveY(){
-        if(yMove < 0){ //Moving up
-            int ty = (int) (y + bounds.getY()) / Settings.TILE_HEIGHT;
-
-            if(!collisionWithTile((int) (x + bounds.getX()) / Settings.TILE_WIDTH, ty) &&
-                    !collisionWithTile((int) (x + bounds.getX() + bounds.getWidth()) / Settings.TILE_WIDTH, ty)){
-                y += yMove/2;
-                yLong += yMove/2;
-            } else {
-                die();
-            }
-        } else if (yMove > 0){ //Moving down
-            int ty = (int) (y + bounds.getY() + bounds.getHeight()) / Settings.TILE_HEIGHT;
-
-            if(!collisionWithTile((int) (x + bounds.getX()) / Settings.TILE_WIDTH, ty) &&
-                    !collisionWithTile((int) (x + bounds.getX() + bounds.getWidth()) / Settings.TILE_WIDTH, ty)){
-                y += yMove/2;
-                yLong += yMove/2;
-            } else {
-                die();
-            }
-        }
-    }
-
-    @Override
-    public void tick() {
-        fire();
-        move();
-        checkHit();
-    }
-
-    public void fire(){
-        if(!isFired){
-            if(direction == 1){ //up
-                yMove = -speed;
-            }
-
-            if(direction == 2){ //down
-                yMove = speed;
-            }
-
-            if(direction == 3){ //left
-                xMove = -speed;
-            }
-
-            if(direction == 4){ //right
-                xMove = speed;
-            }
-            isFired = true;
-        }
-    }
-
-    public void checkHit(){
+	public Bullet(Handler handler, Image image, double x, double y, int damage,int direction) {
+		super(handler, image, x, y, 20,21, damage, 500, 500);
+		this.direction = direction;
+		
+		setSpeed(30);
+		bounds.setX(4);
+		bounds.setY(4);
+		bounds.setHeight(20);
+		bounds.setWidth(20);
+		
+	}
+	@Override
+	public void checkHit(){
         //Enemy hit
         for(Entity e : handler.getWorld().getEntityManager().getEntities()){
             if(e.equals(handler.getWorld().getEntityManager().getPlayer()))
                 continue;
             if(e.getCollisionBounds(0, 0).intersects(getCollisionBounds(0,0).getBoundsInLocal())){
-
-                e.takeDamage(handler.getWorld().getEntityManager().getPlayer().getDamage() + 20);
+                e.takeDamage(damage);
                 die();
+
                 if(!Settings.IS_MUTE){
                     if(Sound.boom.getStatus() == MediaPlayer.Status.PLAYING)
                         Sound.boom.stop();
@@ -128,20 +42,7 @@ public class Bullet extends Creature{
         //Tile hit
 
         //Bullet move
-        if(Math.abs(xLong) > 500 || Math.abs(yLong) > 500)
+        if(Math.abs(xLong) > xFar || Math.abs(yLong) > yFar)
             die();
-    }
-
-    @Override
-    public void render(GraphicsContext g) {
-        g.drawImage(image, (int)(x - handler.getGameCamera().getxOffset()),
-                (int) (y - handler.getGameCamera().getyOffset()));
-
-    }
-
-    @Override
-    public void die() {
-        active = false;
-        System.out.println("Chết :D");
     }
 }
--- src/entities/creatures/Sword.java
+++ src/entities/creatures/Sword.java
@@ -1,114 +1,28 @@
 package entities.creatures;
 
 import entities.Entity;
+import entities.creatures.weapon.Weapon;
 import game.Handler;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.media.MediaPlayer;
 import settings.Settings;
 import sounds.Sound;
 
-public class Sword extends Creature {
+public class Sword extends Weapon {
 
-    private boolean isCut = false;
-    private int xLong = 0, yLong = 0;
-
-    public Sword(Handler handler, Image image, double x, double y, int damage, int direction) {
-        super(handler, image, x, y, 10, 11, damage);
-        this.direction = direction;
-
-        setSpeed(8);
-
-        bounds.setX(0);
-        bounds.setY(0);
-        bounds.setWidth(40);
-        bounds.setHeight(40);
-
-    }
-
-    @Override
-    public void move() {
-        moveX();
-        moveY();
-    }
-
-    public void moveX(){
-        if(xMove > 0){ //Moving right
-            int tx = (int) (x + bounds.getX() + bounds.getWidth()) / Settings.TILE_WIDTH;
-
-            if(!collisionWithTile(tx, (int) (y + bounds.getY()) / Settings.TILE_HEIGHT) &&
-                    !collisionWithTile(tx, (int) (y + bounds.getY() + bounds.getHeight()) / Settings.TILE_HEIGHT)){
-                x += xMove/2;
-                xLong += xMove/2;
-            } else {
-                die();
-            }
-        } else if (xMove < 0){ //Moving left
-            int tx = (int) (x + bounds.getX()) / Settings.TILE_WIDTH;
-
-            if (!collisionWithTile(tx, (int) (y + bounds.getY()) / Settings.TILE_HEIGHT) &&
-                    !collisionWithTile(tx, (int) (y + bounds.getY() + bounds.getHeight()) / Settings.TILE_HEIGHT)) {
-                x += xMove/2;
-                xLong += xMove/2;
-            } else {
-                die();
-            }
-        }
-    }
-
-    public void moveY(){
-        if(yMove < 0){ //Moving up
-            int ty = (int) (y + bounds.getY()) / Settings.TILE_HEIGHT;
-
-            if(!collisionWithTile((int) (x + bounds.getX()) / Settings.TILE_WIDTH, ty) &&
-                    !collisionWithTile((int) (x + bounds.getX() + bounds.getWidth()) / Settings.TILE_WIDTH, ty)){
-                y += yMove/2;
-                yLong += yMove/2;
-            } else {
-                die();
-            }
-        } else if (yMove > 0){ //Moving down
-            int ty = (int) (y + bounds.getY() + bounds.getHeight()) / Settings.TILE_HEIGHT;
-
-            if(!collisionWithTile((int) (x + bounds.getX()) / Settings.TILE_WIDTH, ty) &&
-                    !collisionWithTile((int) (x + bounds.getX() + bounds.getWidth()) / Settings.TILE_WIDTH, ty)){
-                y += yMove/2;
-                yLong += yMove/2;
-            } else {
-                die();
-            }
-        }
-    }
-
-    @Override
-    public void tick() {
-        cut();
-        move();
-        checkHit();
-    }
-
-    public void cut(){
-        if(!isCut){
-            if(direction == 1){ //up
-                yMove = -speed;
-            }
-
-            if(direction == 2){ //down
-                yMove = speed;
-            }
-
-            if(direction == 3){ //left
-                xMove = -speed;
-            }
-
-            if(direction == 4){ //right
-                xMove = speed;
-            }
-            isCut = true;
-        }
-    }
-
-    public void checkHit(){
+	public Sword(Handler handler, Image image, double x, double y, int damage,int direction) {
+		super(handler, image, x, y, 10,11, damage, 23, 23);
+		this.direction = direction;
+		
+		setSpeed(8);
+		bounds.setX(4);
+		bounds.setY(4);
+		bounds.setHeight(40);
+		bounds.setWidth(40);
+		
+	}
+	@Override
+	public void checkHit(){
         //Enemy hit
         for(Entity e : handler.getWorld().getEntityManager().getEntities()){
             if(e.equals(handler.getWorld().getEntityManager().getPlayer()))
@@ -127,21 +41,7 @@ public class Sword extends Creature {
         //Tile hit
 
         //Bullet move
-        if(Math.abs(xLong) > 23 || Math.abs(yLong) > 23)
+        if(Math.abs(xLong) > xFar || Math.abs(yLong) > yFar)
             die();
     }
-
-    @Override
-    public void render(GraphicsContext g) {
-        g.drawImage(image, (int)(x - handler.getGameCamera().getxOffset()),
-                (int) (y - handler.getGameCamera().getyOffset()));
-
-    }
-
-    @Override
-    public void die() {
-        active = false;
-        System.out.println("Chet :D");
-    }
 }
-
